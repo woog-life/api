6f5bdfd2d94cceeb2b280d1df9726e1a23759851
Björn Petersen <git@bjoernpetersen.net>
2021-06-14 15:16:24 +0200

    Send fake event for now

--- lib/src/application/use_case/get_events.dart
+++ lib/src/application/use_case/get_events.dart
@@ -5,6 +5,15 @@ import 'package:woog_api/src/domain/model/event.dart';
 class GetEvents {
   Future<List<Event>> call(String lakeId) async {
     // TODO: implement
-    return [];
+    return [
+      Event(
+        variation: "Badestelle Familienbad",
+        bookingLink: "https://ztix.de/hp/events/3834/info",
+        beginTime: DateTime.now().toUtc(),
+        endTime: DateTime.now().toUtc().add(Duration(hours: 5)),
+        saleStartTime: DateTime.now().toUtc(),
+        isAvailable: true,
+      )
+    ];
   }
 }
