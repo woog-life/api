884f5f6b253c3249e9bed291f92c3bbc6698ba5d
Björn Petersen <git@bjoernpetersen.net>
2023-06-24 14:32:36 +0200

    Handle SIGINT and SIGTERM

--- lib/src/infrastructure/api/woog_api.dart
+++ lib/src/infrastructure/api/woog_api.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:injectable/injectable.dart';
+import 'package:logger/logger.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:woog_api/src/infrastructure/api/dispatcher.dart';
@@ -15,12 +16,14 @@ class WoogApi {
   final LoggingMiddleware _loggingMiddleware;
   final SentryMiddleware _sentryMiddleware;
   final Dispatcher _dispatcher;
+  final Logger _logger;
   late final Handler handler;
 
   WoogApi(
     this._loggingMiddleware,
     this._sentryMiddleware,
     this._dispatcher,
+    this._logger,
   ) {
     handler = const Pipeline()
         .addMiddleware(_sentryMiddleware)
@@ -29,7 +32,30 @@ class WoogApi {
         .addHandler(_dispatcher);
   }
 
+  void _runOnStopSignals(Future Function(ProcessSignal signal) action) {
+    final streams = [
+      ProcessSignal.sigint.watch(),
+      if (!Platform.isWindows) ProcessSignal.sigterm.watch(),
+    ];
+
+    final subscriptions = <StreamSubscription>[];
+
+    for (final stream in streams) {
+      subscriptions.add(stream.listen((signal) {
+        for (final sub in subscriptions) {
+          sub.cancel();
+        }
+        action(signal);
+      }));
+    }
+  }
+
   Future<void> launch() async {
-    await io.serve(handler, InternetAddress.anyIPv4, 8080);
+    final server = await io.serve(handler, InternetAddress.anyIPv4, 8080);
+    _runOnStopSignals((signal) async {
+      _logger.w('Server is stopping because of a ${signal.name} signal');
+      await server.close();
+      _logger.i('Server is closed.');
+    });
   }
 }
