7543febeff3faa84cee72735d095f6d0803855b9
Björn Petersen <git@bjoernpetersen.net>
2023-09-23 13:44:57 +0200

    Update lake names

---
+++ migrations/sql/V10__ImproveLakeNames.sql
@@ -0,0 +1,39 @@
+update lake
+set name = 'Alster (Hamburg)'
+where id = '55e5f52a-2de8-458a-828f-3c043ef458d9';
+
+update lake
+set name = 'Havel (Potsdam)'
+where id = 'bedbdac7-7d61-48d5-b1bd-0de5be25e953';
+
+update lake
+set name = 'Atlantik (Santander)'
+where id = 'a2595d6a-a6fc-4ee3-86b4-871f32f28b4c';
+
+update lake
+set name = 'Mittelmeer (Heraklion)'
+where id = '359e0773-e7ee-4ee0-8c11-a9eb5082d899';
+
+update lake
+set name = 'Nordsee (Cuxhaven)'
+where id = 'd074654c-dedd-46c3-8042-af55c93c910e';
+
+update lake
+set name = 'Nordsee (Husum)'
+where id = '18e6931a-3729-4ad9-8301-03c5980f82b6';
+
+update lake
+set name = 'Pazifik (Vancouver)'
+where id = 'fb086a0d-dc93-40fc-ad41-b6dbe0358f0b';
+
+update lake
+set name = 'Aare (Bern-Schönau)'
+where id = 'acf32f07-e702-4e9e-b766-fb8993a71b21';
+
+update lake
+set name = 'Großer Woog (Darmstadt)'
+where id = '69c8438b-5aef-442f-a70d-e0d783ea2b38';
+
+update lake
+set name = 'Arheilger Mühlchen (Darmstadt)'
+where id = '25aa2968-e34e-4f86-87cc-56b16b5aff36';
