f273d484832b4c78ab46020abd2c4873c9e2ecb3
Björn Petersen <git@bjoernpetersen.net>
2023-11-27 15:23:00 +0100

    Fix sorrento migration

--- migrations/sql/V13__AddLakeSorrento.sql
+++ migrations/sql/V13__AddLakeSorrento.sql
@@ -1,3 +1,2 @@
-insert into lake (id, name, supports_temperature, supports_tides)
-values ('4ddb043b-d0d2-44a5-b321-6efbaacf98a0', 'Mittelmeer (Sorrent)', true, false)
-on conflict do nothing;
+insert into lake (id, name, time_zone_id, supports_temperature, supports_tides)
+values ('4ddb043b-d0d2-44a5-b321-6efbaacf98a0', 'Mittelmeer (Sorrent)', 'Europe/Rome', true, false);
