8ab10d353fb78a1b665041b4d380e491f4a0159c
Torben Carstens <git@carstens.tech>
2023-11-27 15:03:35 +0100

    Update migrations/sql/V11_AddLakeSorrento.sql
    Co-authored-by: Björn Petersen <git@bjoernpetersen.net>

--- migrations/sql/V11_AddLakeSorrento.sql
+++ migrations/sql/V11_AddLakeSorrento.sql
@@ -1,3 +1,3 @@
 insert into lake (id, name, supports_temperature, supports_tides)
-values ('4ddb043b-d0d2-44a5-b321-6efbaacf98a0', 'Tyrrhenisches Meer (Sorrento)', true, false)
+values ('4ddb043b-d0d2-44a5-b321-6efbaacf98a0', 'Mittelmeer (Sorrent)', true, false)
 on conflict do nothing;
